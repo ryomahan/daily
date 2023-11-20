05d023df2b6e4697a4d2592bf0f7c29e8bea0f36
ryomahan <ryomahan1996@gmail.com>
2023-11-20 15:16:03 +0800

    watch less and rander to css real-time

--- scripts/dev.js
+++ scripts/dev.js
@@ -3,16 +3,57 @@ const bs = require("browser-sync").create()
 const path = require("path")
 const lessc = require("less")
 const rootPath = path.resolve(__dirname, "..")
+const tailwind = require("tailwindcss/lib/cli/build");
 
+
+function renderCSS() {
+  fs.readFile(path.join(rootPath, "/tailwind.less"), (err, data) => {
+    lessc.render(data.toString(), (e, css) => {
+      if (e) return;
+      fs.writeFile(path.join(rootPath, "/style.css"), css.css.toString(), async () => {
+        try {
+          await tailwind.build({
+            "--input": path.join(rootPath, "/style.css"),
+            "--output": path.join(rootPath, "/style.css"),
+          })
+        } catch {}
+      })
+    })
+  })
+}
 
 fs.readFile(path.join(rootPath, "/tailwind.less"), (err, data) => {
   lessc.render(data.toString(), (e, css) => {
-    fs.writeFile(path.join(rootPath, "/style.css"), css.css.toString(), () => {})
-    // tailwindcss.run("-i style.css")
+    if (e) {
+      bs.watch(["tailwind.less"], {}, function () {
+        renderCSS()
+      })
+
+      bs.init({
+        ui: false,
+        open: false,
+        server: false
+      })
+    } else {
+      fs.writeFile(path.join(rootPath, "/style.css"), css.css.toString(), async () => {
+        try {
+          await tailwind.build({
+            "--input": path.join(rootPath, "/style.css"),
+            "--output": path.join(rootPath, "/style.css"),
+          })
+        } finally {
+
+          bs.watch(["tailwind.less"], {}, function () {
+            renderCSS()
+          })
+
+          bs.init({
+            ui: false,
+            open: false,
+            server: false
+          })
+        }
+      })
+    }
   })
 })
-// bs.watch(["tailwind.less"], {}, function () {
-  // lessc.render
-// })
-
-// bs.init({ ui:false, open: false, server: false })
